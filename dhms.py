4083bd56967d4dd8c0b8087395eda7eacbfe9773
AloshynYaroslav <72693406+AloshynYaroslav@users.noreply.github.com>
2020-10-21 00:05:07 +0300

    Update dhms.py

--- dhms.py
+++ dhms.py
@@ -6,7 +6,7 @@ def mu_func3():
     c = int((a - 86400) / 3600)
     d = int((a - (86400 + c * 3600)) / 60)
     e = int(a - (b * 86400) - (c * 3600) - (d * 60))
-    print(b, "d,", c, "h", d, "m", e, "s")
+    print(b, "d,", c, "h,", d, "m,", e, "s")
 
 
 mu_func3()
