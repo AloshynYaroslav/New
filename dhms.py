61fa3e234b313c4b6406b898e4f7ddba2428fd30
AloshynYaroslav <72693406+AloshynYaroslav@users.noreply.github.com>
2020-10-21 00:02:43 +0300

    Add files via upload

---
+++ dhms.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+
+def mu_func3():
+    b = int(a / 86400)
+    c = int((a - 86400) / 3600)
+    d = int((a - (86400 + c * 3600)) / 60)
+    e = int(a - (b * 86400) - (c * 3600) - (d * 60))
+    print(b, "d,", c, "h", d, "m", e, "s")
+
+
+mu_func3()
